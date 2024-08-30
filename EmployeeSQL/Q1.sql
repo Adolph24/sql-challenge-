5527b3cda5d4ae7c7c2a53e9f352b31268ab3848
Adolphus Momoh, Jr <167161826+Adolph24@users.noreply.github.com>
2024-08-29 23:22:58 -0500

    Update Q1.sql

--- EmployeeSQL/Q1.sql
+++ EmployeeSQL/Q1.sql
@@ -1,5 +1,6 @@
+Listing the following employee number, lastname, firstname, sex, and salary ofeach employee.
 
-
+  
 CREATE VIEW Q1 AS 
 SELECT e.emp_no, e.last_name,e.first_name,e.sex,s.salary
 FROM employees AS e
